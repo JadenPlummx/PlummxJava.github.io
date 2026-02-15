1e8e96e0298d0bc1e14d710b6c05d821ec999f97
JadenPlummx <jaden.plummer@dcmail.ca>
2026-02-14 22:08:03 -0500

    Uploaded .Java file
    includes name, date and description.

---
+++ PizzaSlicesCalculator.java
@@ -0,0 +1,10 @@
+/*
+ * Name: Jaden Plummer
+ * Date: 2026-02-14
+ * Description: This program calculates how many slices a pizza should be cut into
+ *              based on its diameter. It also calculates the area of each slice.
+ */
+
+
+public class PizzaSlicesCalculator {
+}
